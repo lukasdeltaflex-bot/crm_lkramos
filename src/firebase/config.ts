10bdbf36102e1d2fd5223f6a6dae4428416baaed
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-01-14 21:00:04 +0000

    const firebaseConfig = {   apiKey: "...",   authDomain: "...",   project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  "projectId": "studio-248448941-9c1c2",
+  "projectId": "XXXXXXX",
   "appId": "1:341426752875:web:348f88597e5b9b2057d02e",
   "apiKey": "AIzaSyDcdnNBy0TZTsq_cI02KFVU9o7PJopEczM",
   "authDomain": "studio-248448941-9c1c2.firebaseapp.com",
