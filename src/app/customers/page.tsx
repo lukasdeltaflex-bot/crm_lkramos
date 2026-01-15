84425b52ef71b485ea8416f2cc931f55101baee6
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-01-15 13:28:36 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogTrigger is no

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { customers as sampleCustomers, proposals as sampleProposals } from '@/lib/data';
 
