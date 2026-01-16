c07b8c221ba11e7da87b53801b3185e61c1c9bf1
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-01-16 19:12:00 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "XXXXXXX",
+  "projectId": "studio-248448941-9c1c2",
   "appId": "1:341426752875:web:348f88597e5b9b2057d02e",
   "apiKey": "AIzaSyDcdnNBy0TZTsq_cI02KFVU9o7PJopEczM",
   "authDomain": "studio-248448941-9c1c2.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-TX4KQ9R6TL",
   "messagingSenderId": "341426752875"
 };
