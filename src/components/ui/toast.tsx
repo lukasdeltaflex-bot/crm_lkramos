d224596c097e0b0a72845e3702c5b16b706ac0e7
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-01-27 13:49:21 +0000

    gostaria que essas notificações tivesse ou um fundo ou um relevo de cor

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -25,13 +25,13 @@ const ToastViewport = React.forwardRef<
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName
 
 const toastVariants = cva(
-  "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none sm:max-w-[420px]",
+  "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none sm:max-w-[420px] border-l-4",
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border-l-primary bg-background text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "border-destructive bg-destructive text-destructive-foreground border-l-destructive-foreground",
       },
     },
     defaultVariants: {
