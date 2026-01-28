6ad639d149c5ac29dbe48f64f3cb5434f5637917
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-01-28 14:17:09 +0000

    AS MENSAGENS DE NOTIFICAÇÃO ESTÃO DEMORANDO MUITO PARA DESAPARECER

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -24,7 +24,7 @@ export function Toaster() {
   }
 
   return (
-    <ToastProvider>
+    <ToastProvider duration={3000}>
       <div className="space-y-2">
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
