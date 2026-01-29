97fb9b574c6dd32b1dc0db910c6023ad5de51dae
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-01-29 17:07:05 +0000

    nao funcionou!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,20 +85,10 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
-        'in': {
-          from: { opacity: '0', transform: 'translateY(8px) scale(0.98)' },
-          to: { opacity: '1', transform: 'translateY(0) scale(1)' },
-        },
-        'out': {
-          from: { opacity: '1', transform: 'translateY(0) scale(1)' },
-          to: { opacity: '0', transform: 'translateY(8px) scale(0.98)' },
-        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'in': 'in 0.2s ease-out',
-        'out': 'out 0.2s ease-in',
       },
     },
   },
